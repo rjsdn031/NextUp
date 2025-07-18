07e3801e10370d24669ef7f45ab12ae469eb0fed
rjsdn031 <geonwoo031@gmail.com>
2025-07-18 15:54:27 +0900

    [fix] Mod Alarm Model

--- lib/models/alarm_model.dart
+++ lib/models/alarm_model.dart
@@ -9,9 +9,21 @@ class AlarmModel {
   final List<DayOfWeek> days;
   bool enabled;
 
+  final String name;
+  final bool skipHolidays;
+  final bool vibration;
+  final bool snoozeEnabled;
+  final String ringtone;
+
   AlarmModel({
     required this.time,
     required this.days,
     this.enabled = true,
+
+    this.name = '',
+    this.skipHolidays = false,
+    this.vibration = true,
+    this.snoozeEnabled = true,
+    this.ringtone = 'null',
   });
 }
