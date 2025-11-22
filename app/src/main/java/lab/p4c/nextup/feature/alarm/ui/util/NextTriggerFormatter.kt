752669802ab5da6cf82a103cb3168c77a85269cf
rjsdn031 <geonwoo031@gmail.com>
2025-11-22 17:20:35 +0900

    [fix] Fix NextAlarm Text

--- app/src/main/java/lab/p4c/nextup/feature/alarm/ui/util/NextTriggerFormatter.kt
+++ app/src/main/java/lab/p4c/nextup/feature/alarm/ui/util/NextTriggerFormatter.kt
@@ -51,26 +51,29 @@ object NextTriggerFormatter {
         val zone = now.zone
         val trigger = Instant.ofEpochMilli(triggerAtUtcMillis).atZone(zone)
 
-        val dayDiff = Duration.between(
-            now.toLocalDate().atStartOfDay(zone),
-            trigger.toLocalDate().atStartOfDay(zone)
-        ).toDays()
-
-        val datePart = when (dayDiff) {
-            0L -> ""    // 오늘이면 datePart Skip
-            1L -> "내일"
-            else -> "${dayDiff}일 후"
-        }
+        val startOfToday = now.toLocalDate().atStartOfDay(zone)
+        val startOfTriggerDay = trigger.toLocalDate().atStartOfDay(zone)
+        val dayDiff = Duration.between(startOfToday, startOfTriggerDay).toDays()
 
         val diff = Duration.between(now, trigger)
         val hours = diff.toHours()
         val minutes = diff.minusHours(hours).toMinutes()
-        val remain = if (hours == 0L && minutes == 0L) "잠시 후" else buildString {
-            if (hours > 0) append("${hours}시간 ")
-            append("${minutes}분 후")
+
+        val remainText = when {
+            hours == 0L && minutes == 0L -> "잠시 후"
+            hours == 0L -> "${minutes}분 후"
+            else -> "${hours}시간 ${minutes}분 후"
         }
 
-        val text = if (datePart != "") datePart else remain
+        val datePart = when {
+            dayDiff == 0L -> "" // 오늘은 datePart skip
+            dayDiff == 1L && diff.toHours() < 24 -> "" // 내일이어도 24시간 이하면 remain 표시
+            dayDiff == 1L -> "내일"
+            else -> "${dayDiff}일 후"
+        }
+
+        val text = datePart.ifEmpty { remainText }
+
         return "$text\n알람이 울립니다"
     }
 
