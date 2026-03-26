fc8a3030b8024ca98e1679053a98d02d32d5e192
rjsdn031 <geonwoo031@gmail.com>
2026-03-26 19:04:58 +0900

    [chore] Update App Version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -18,8 +18,8 @@ android {
         applicationId = "lab.p4c.nextup"
         minSdk = 30
         targetSdk = 36
-        versionCode = 4
-        versionName = "1.0.5"
+        versionCode = 5
+        versionName = "1.0.6"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
